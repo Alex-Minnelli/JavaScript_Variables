d81f28b8f632f37eb582d89759f16e2457f5d7f3
Alex Minnelli <alexminnelli@gmail.com>
2022-10-20 10:44:12 -0400

    exercises

--- app.js
+++ app.js
@@ -4,7 +4,35 @@ console.log(
 );
 
 // Exercise 1
+const firstName = '';
+let lastName = 'Alex Minnelli';
+var age = 23;
+
 
 // Exercise 2
+firstName = 'Alex';
+lastName = 'Minnelli';
+age = 23;
+
 
 // Exercise 3
+let language = 'JavaScript';
+let createdYear = 1995;
+let isCaseSenesitive = True;
+
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
